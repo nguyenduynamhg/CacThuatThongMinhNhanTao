b527eba0796076ca4cede07c9464f81d42cd7acd
Nguyen Duy Nam <50732362+nguyenduynamhg@users.noreply.github.com>
2021-01-03 11:38:00 +0700

    bfs nay.bug chua fix

--- BFS.py
+++ BFS.py
@@ -1,23 +1,18 @@
-def bfs(matrix, row, col, visited):
-    nodes = [(row, col)]
-    while nodes:
-        row, col = nodes.pop(0)
-        if row >= len(matrix) or col >= len(matrix[0]) or row < 0 or col < 0 and matrix[row][col]==3:
-            continue
-        if (row, col) not in visited:
-            if matrix[row][col] == 0:
-                visited.append((row, col))
-                nodes.append((row + 1, col))
-                nodes.append((row, col + 1))
-                nodes.append((row, col - 1))
-def bfs_wrapper(matrix):
-    visited = []
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
+def dfs(current_path):
+    row, col = current_path[-1]
+    if (row, col) == goal_state:
+        return True
 
-            if (i, j) not in visited:
-                bfs(matrix, i, j, visited)
-                matrix[i, j] == 9  # danh dau duong di
-                print('[(', i, j, ')]')
+    for direction in [(row, col + 1),  (row + 1, col),(row+1,col)]:
+        new_row, new_col = direction
 
-    return visited
+        if (0 <= new_row < num_rows and 0 <= new_col < num_cols and
+                matrix[new_row][new_col] ==0 and
+                (new_row, new_col) not in current_path):
+            current_path.append((new_row, new_col))
+            matrix[new_row, new_col] == 'X'  # danh dau duong di
+            print('[(', new_row, new_col, ')]')
+            if dfs(current_path):
+                return True
+            else:
+                current_path.pop()
